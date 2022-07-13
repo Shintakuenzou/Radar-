eedb589d3820cb4c047bc4ba559cf667b23b108b
Felipe Enzou <91697813+Shintakuenzou@users.noreply.github.com>
2022-07-13 13:16:52 -0300

    Delete exercicio03.js

--- Js/exercicio03.js
+++
@@ -1,6 +0,0 @@
-
-function exibirNome() {
-    var nome = document.getElementById('nome').value
-    alert("O nome desse usuário é: " + nome)
-    alert("A quantidade das letras do nome é: " + nome.length)
-}
