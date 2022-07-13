d859388551439e6e920c0841af93d150958479d9
Felipe Enzou <91697813+Shintakuenzou@users.noreply.github.com>
2022-07-13 13:16:58 -0300

    Delete exercicio04.js

--- Js/exercicio04.js
+++
@@ -1,7 +0,0 @@
-function desligarLampada () {
-    document.getElementById('lampada').src = "../image/lampada.jpg"
-}
-
-function acenderLampada() {
-    document.getElementById('lampada').src = "../image/lampada-on.jpg"
-}
